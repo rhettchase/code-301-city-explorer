bcf0c8dbefdb7205e5395d23a3cee5a79ea9b328
RHETT CHASE <rhettjchase@gmail.com>
2023-12-06 20:33:10 -0800

    updates card styling for movie render

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,24 +1,30 @@
 import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Movies( {movies, location} ) {
   return (
     <div className='movies'>
-        {movies.length > 0 && (
-        <h2>Movies with {location.display_name} in the name:</h2>
-      )}
+    {movies.length > 0 && (
+      <h2>Movies with {location.display_name} in the name:</h2>
+    )}
+    <Row xs={1} md={2} lg={3} xl={4} className='g-4'>
       {movies.map((movie, index) => (
-        <Card key={index} style={{ width: '18rem' }}>
-          <Card.Img variant='top' src={movie.image_url} alt={movie.title} />
-          <Card.Body>
-            <Card.Title>{movie.title}</Card.Title>
-            <Card.Text>{movie.overview}</Card.Text>
-            <Card.Text>Average Votes: {movie.average_votes}</Card.Text>
-            <Card.Text>Total Votes: {movie.total_votes}</Card.Text>
-            <Card.Text>Popularity: {movie.popularity}</Card.Text>
-            <Card.Text>Released On: {movie.released_on}</Card.Text>
-          </Card.Body>
-        </Card>
+        <Col key={index}>
+          <Card style={{ width: '25rem' }}>
+            <Card.Img variant='top' src={movie.image_url} alt={movie.title} />
+            <Card.Body>
+              <Card.Title>{movie.title}</Card.Title>
+              <Card.Text>{movie.overview}</Card.Text>
+              <Card.Text>Average Votes: {movie.average_votes}</Card.Text>
+              <Card.Text>Total Votes: {movie.total_votes}</Card.Text>
+              <Card.Text>Popularity: {movie.popularity}</Card.Text>
+              <Card.Text>Released On: {movie.released_on}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
       ))}
-    </div>
+    </Row>
+  </div>
   );
 }
